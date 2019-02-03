9e6e56f29a170713873afe64083f1f37cb0fca9f
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-02-02 18:32:48 -0700

    working APIs

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# liri-robot
-Language Interpretation &amp; Recognition Interface using node.js
+# Liri Node App
+Language Interpretation and Recognition Interface using node.js
+
+
+This is a CLI tool that will assist in doing the following basic searches:
+Search on Bands In Town for a band and list any upcoming events
+Search on Spotify for a specific song
+Search on the Open Movies Database for a specific movie.
+
+Instructions:
+Clone repo.
+
+Add .env file in the root directory with your spotify API keys like this: SPOTIFY_ID= SPOTIFY_SECRET=
+
+Install node package dependencies (if using npm: npm install)
+
+Run the tool with the following command: node liri.js
+
+the following commands are available:
+concert-this
+spotify-this-song
+movie-this
+do-what-it-says
+
+Optional:
+There is a do-what-it-says command feature. This feature will automatically read the random.txt file and execute the command search in it.
+
+
+Additional Notes:
+Please view screen shots of functioning app saved in the images folder
--- liri.js
+++ liri.js
@@ -1,2 +1,111 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
+var fs = require('fs');
+var moment = require("moment");
+
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api')
+var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+
+var nodeArgs = process.argv;
+var command = nodeArgs[2];
+
+
+var artist = "";
+var movie = "";
+var song = "";
+
+//For loop to handle multiple word searches
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        artist = artist + "+" + nodeArgs[i];
+        movie = movie + "+" + nodeArgs[i];
+        song = song + "+" + nodeArgs[i];
+    }
+    else {
+        artist += nodeArgs[i];
+        movie += nodeArgs[i];
+        song += nodeArgs[i];
+    }
+};
+
+//concert-this command using Bands in Town API
+if (command === "concert-this") {
+
+    var bandsUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    console.log(bandsUrl);
+
+    axios.get(bandsUrl)
+        .then(function(response) {
+                
+                console.log("\nThe following event was found for " + artist + ":");
+                console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+                console.log("When: " + moment(response.data[0].datetime).format('MM/DD/YYYY'));
+                console.log("Venue: " + response.data[0].venue.name);
+                console.log("--------------------------------\n");
+            })
+
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
+//spotify-this-song command using Spotify API
+if (command === "spotify-this-song") {
+    
+    spotify.search( {type: 'track', query: song} )
+
+    .then(function(response) {
+        
+        console.log("\nYour Spotify Song Search Results:");
+        console.log("Artist: " + response.tracks.items[0].artists[0].name);
+        console.log("Track Name: " + response.tracks.items[0].name);        
+        console.log("Album Name: " + response.tracks.items[0].album.name);
+        console.log("URL to Hear Song: " + response.tracks.items[0].external_urls.spotify);
+        console.log("--------------------------------\n");
+
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+};
+
+
+//movie-this command using OMDB API
+if (command === "movie-this") {
+    var omdbUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+    console.log(omdbUrl);
+
+    axios.get(omdbUrl)
+        .then(function(response){
+            console.log("\nYour movie search results:\n")
+            console.log("Title: " + response.data.Title);
+            console.log("Release Date: " + response.data.Released);
+            console.log("Country of Production: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Actors/Actresses: " + response.data.Actors);
+            console.log("Plot: " + response.data.Plot);            
+            console.log("IMDB Rating: " + response.data.imdbRating);
+
+            if (response.data.Ratings[1].Value === null) {
+                console.log("Rotten Tomatoes Rating: N/A")
+            }
+            else {
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)};
+            console.log("----------------------------------------\n");
+        })
+        
+        .catch(function (error) {
+            console.log(error);
+        });
+        
+    
+};
+
+
+
+
+
+
 
